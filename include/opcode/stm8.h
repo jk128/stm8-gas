e05642053454b177c70154e7f5758c9a3fed1ccc
Valentin Dudouyt <valentin.dudouyt@gmail.com>
2012-09-07 09:29:53 +0700

    s/ST8_SHORTMEM/ST8_SPREL/

--- include/opcode/stm8.h
+++ include/opcode/stm8.h
@@ -2,14 +2,14 @@
 {"incw", {ST8_REG_X, ST8_END}, 3, 0x5C},
 {"incw", {ST8_REG_Y, ST8_END}, 3, 0x905C},
 /* logical operators */
-{"or", {ST8_BYTE, ST8_END}, 2, 0xAA},
-{"or", {ST8_SHORTMEM, ST8_END}, 2, 0xBA},
-{"or", {ST8_LONGMEM, ST8_END}, 3, 0xCA},
+{"or", {ST8_REG_A, ST8_BYTE, ST8_END}, 2, 0xAA},
+{"or", {ST8_REG_A, ST8_SPREL, ST8_END}, 2, 0xBA},
+{"or", {ST8_REG_A, ST8_LONGMEM, ST8_END}, 3, 0xCA},
 /* ld / mov */
 {"ld", {ST8_REG_A, ST8_BYTE, ST8_END}, 2, 0xA6},
 {"ldw", {ST8_REG_X, ST8_WORD, ST8_END}, 3, 0xAE},
 /* comparison / jumps */
 {"cp", {ST8_REG_A, ST8_BYTE, ST8_END}, 3, 0xA1},
 {"cpw", {ST8_REG_X, ST8_WORD, ST8_END}, 3, 0xA3},
-{"jrnc", {ST8_SHORTMEM, ST8_END}, 3, 0x24},
-{"jpf", {ST8_EXTMEM, ST8_END}, 3, 0xAC},
+{"jrnc", {ST8_SPREL, ST8_END}, 4, 0x24},
+{"jpf", {ST8_EXTMEM, ST8_END}, 4, 0xAC},
